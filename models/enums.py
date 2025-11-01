b8a959f67b11a6ae80b4108c447842a0997c0ac8
pyhcho099 <155559163+pyhcho099@users.noreply.github.com>
2025-11-01 09:56:06 +0530

    Update enums.py

--- models/enums.py
+++ models/enums.py
@@ -1,1 +1,23 @@
-.
+from enum import Enum
+
+class ContractRole(str, Enum):
+    RP = 'rp'
+    TL = 'tl'
+    PR = 'pr'
+    CLRD = 'clrd'
+    TS = 'ts'
+    QC = 'qc'
+    UPLOADER = 'uploader'
+
+class TransactionType(str, Enum):
+    EARN = 'earn'
+    SPEND = 'spend'
+    TRANSFER = 'transfer'
+    PENALTY = 'penalty'
+    REFUND = 'refund'
+
+class BountyStatus(str, Enum):
+    OPEN = 'open'
+    CLAIMED = 'claimed'
+    RESOLVED = 'resolved'
+    CANCELLED = 'cancelled'
